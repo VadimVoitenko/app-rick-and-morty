c59c8a9d5b760bfdf86867296b30fcb6665b264d
Vadim Voitenko <mirvoy8031@gmail.com>
2024-09-23 00:10:29 +0300

    feat: add header

--- src/app/modules/home/pages/heroes/heroes.component.ts
+++ src/app/modules/home/pages/heroes/heroes.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../../shared/modules/layout/components/header/header.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+  styleUrl: './heroes.component.scss',
 })
-export class HeroesComponent {
-
-}
+export class HeroesComponent {}
